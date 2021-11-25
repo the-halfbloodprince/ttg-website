dbdb9ec23e19cfd68f1acc711e375f26edf3c3af
Tejas Ambhorer <TejasAm2020@gmail.com>
2021-11-25 22:37:06 +0530

    error

--- src/components/eventdata.js
+++ src/components/eventdata.js
@@ -1,24 +1,7 @@
-const eventData = [
-    { 
-        imageName: 'arm.jpg',
-        event:"Annual Research Meet",
-        content:"Annual Research Meet is the initiative by TTG to explain research and development going on in IIT Kharagpur.Technology Transfer Group started a new initiative to connect the research and academia of IIT Kharagpur with the industry and reach out to the students. We discuss new technologies and research culture in college.  ",
-        heading:"ANNUAL Research MEET" },
-    { 
-        imageName: 'ipr.jpg',
-        event: "IPR",
-        content:"ipr is a ",
-        heading:"IPR Workshop" },
-	{ 
-        imageName: 'tedx.jpg',
-        event: "TEDxIITKharagpur",
-        content:"TEDxIITKharagpur independently organized TED events (subject to certain rules and regulations). TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. At a TEDx event, TEDTalks videos combine to spark deep discussion and connection in a small group. Last year Alyssa Carson, Amit Ranjan Biswas, Debayan Saha, Nabila Jamshed, Siddhant Shah gave talks in an online Tedx event. ",
-        heading:"TEDx IIT Kharagpur" },
-    { 
-        imageName: 'techtalks.jpg',
-        event: "TechTalks",
-        content:"Tech Talks is an interactive session with experts on various interesting topics.We will be discussing interesting topics with the experts working in that field. These will include tech and non-tech topics as well. These will be informative sessions where many students can get knowledge of topics outside of their curriculum also!! ",
-        heading:"Tech Talks" },
+export default [
+    {   imageName: 'arm.jpg', event:"Annual Research Meet",content:"Annual Research Meet is the initiative by TTG to explain research and development going on in IIT Kharagpur.Technology Transfer Group started a new initiative to connect the research and academia of IIT Kharagpur with the industry and reach out to the students. We discuss new technologies and research culture in college.  ",heading:"ANNUAL Research MEET" },
+    {  imageName: 'ipr.jpg', event: "IPR",content:"ipr is a ",heading:"IPR Workshop" },
+	{  imageName: 'tedx.jpg', event: "TEDxIITKharagpur",content:"TEDxIITKharagpur independently organized TED events (subject to certain rules and regulations). TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. At a TEDx event, TEDTalks videos combine to spark deep discussion and connection in a small group. Last year Alyssa Carson, Amit Ranjan Biswas, Debayan Saha, Nabila Jamshed, Siddhant Shah gave talks in an online Tedx event. ",heading:"TEDxIITKharagpur" },
+    {  imageName: 'techtalks.jpg', event: "TechTalks",content:"Tech Talks is an interactive session with experts on various interesting topics.We will be discussing interesting topics with the experts working in that field. These will include tech and non-tech topics as well. These will be informative sessions where many students can get knowledge of topics outside of their curriculum also!! ",heading:"Tech Talks" },
+    {  imageName: 'indac.jpg', event: "INDAC",content:"indac is a ",heading:"INDAC" }
 	];
-
-export default eventData;
